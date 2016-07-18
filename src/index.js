10f6f92f5b3e1a70a2381f234dc65a756c3c9ab9
MesosTables <carwbrown@gmail.com>
2016-07-18 17:31:33 -0400

    pushing

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+
 //router will decide what to show on the screen
 //browserHistory tells react-router how to interpret changes to the URL
 import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
+
+//defined in routes.js
 import routes from './routes';
 import promise from 'redux-promise';
 
